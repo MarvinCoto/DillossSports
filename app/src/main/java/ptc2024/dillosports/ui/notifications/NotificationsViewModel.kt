b8bbfa3fd9cdf80c9ce2c7d641f84aa6bd23a466
Marvin Coto <20230644@ricaldone.edu.sv>
2024-06-19 18:39:31 -0600

    Initial Commit

---
+++ app/src/main/java/ptc2024/dillosports/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ptc2024.dillosports.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
